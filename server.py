385138a1e456dcf131e863d3b73332b31f2d7ba5
BryantCR <bryancvargas20@gmail.com>
2021-09-23 16:32:44 -0600

    ok all

--- server.py
+++ server.py
@@ -7,16 +7,36 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def home():
     return 'Welcome'        # Return the string 'Hello World!' as a response
 
-@app.route('/play', methods = ['GET'])
-def play():
-    return render_template( 'index.html' )
+#@app.route('/play', methods = ['GET'])
+#def play():
+#    return render_template( 'index.html' )
 
-@app.route('/play/<number>/<color>')
-def playNumber(number, color):
-    bNumber = int(number)
+@app.route('/play/<num>/<color>', methods = ['GET'])
+def playNumber(num, color):
+    bNumber = int(num)
     bColor = str(color)
-    return render_template( 'index.html', bNumber, bColor)
+    return render_template ( 'index.html', numbers = bNumber, color1 = bColor)
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
+
+
+# from flask import Flask
+# from flask import render_template
+
+# app = Flask( __name__ )
+
+
+# @app.route ('/play/<num>/<color>' ,methods=['GET'])
+# def boxes(num,color):
+#     numberofboxes = int(num)
+#     colorofboxes = str(color)
+    
+
+
+#     return render_template ('index.html', number = numberofboxes, color = colorofboxes)
+
+
+# if __name__ == "__main__":
+#     app.run( debug = True )
